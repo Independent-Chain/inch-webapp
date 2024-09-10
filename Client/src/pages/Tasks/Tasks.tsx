6a1e394441076cea915ca46ea455d103c48095b9
dev.inch <diomin.vd13@icloud.com>
2024-09-10 20:38:53 +0300

    add more information in tasks

--- Client/src/pages/Tasks/Tasks.tsx
+++ Client/src/pages/Tasks/Tasks.tsx
@@ -18,13 +18,11 @@ const Tasks = ({}: ComponentProps): JSX.Element => {
 
 	return (
 		<div className="page" id="tasks">
-			<Title plain level={3} weight={2} color="white" center>{ localization.tasks.title }</Title>
-			<Caption color="gray">
-				<p style={ {textAlign: 'center'} }>
-					{ localization.tasks.cooperation } 
-					<a href="https://t.me/inch_support" style={ {color: 'var(--accent-1000'} }>@inch_support</a>
-				</p>
-			</Caption>
+			<p className="tasks-title">{ localization.tasks.title }</p>
+			<p className="tasks-description" style={ {textAlign: 'center'} }>
+				{ localization.tasks.cooperation } 
+				<a href="https://t.me/inch_support" style={ {color: 'var(--accent-1000'} }>@inch_support</a>
+			</p>
 		</div>
 	)
 }
