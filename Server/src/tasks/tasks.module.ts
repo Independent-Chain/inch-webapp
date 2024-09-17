af8d96af785cd0d1940fa38925624ba1c2ffe02a
dev.inch <diomin.vd13@icloud.com>
2024-09-17 22:06:56 +0300

    add prisma provider in tasks.modules.ts

--- Server/src/tasks/tasks.module.ts
+++ Server/src/tasks/tasks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [PrismaService, TasksService],
 })
 export class TasksModule {}
