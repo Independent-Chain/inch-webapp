e6a359bdb7979828958f732db4ce8e87f4342dbb
dev.inch <diomin.vd13@icloud.com>
2024-09-18 21:54:22 +0300

    config server side for debug mode

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -3,16 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = process.env.ALLOW_DOMAINS.split(',')
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(null, false);
-      }
-    },
-  });
+  app.enableCors() // Row for debug mode (allow all CORS);
+  // app.enableCors({
+  //   origin: (origin, callback) => {
+  //     const allowedOrigins = process.env.ALLOW_DOMAINS.split(',')
+  //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(null, false);
+  //     }
+  //   },
+  // });
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
