85cee18ad67c65b3eb5eda8e6916b875233dcd71
dev.inch <diomin.vd13@icloud.com>
2024-09-22 20:08:13 +0300

    update api.config.js for easely develop

--- Client/src/api/api.config.js
+++ Client/src/api/api.config.js
@@ -1,8 +1,16 @@
 // Develop: http://localhost:3000/api
 // Production: https://inch-app.ru/api
 
-const apiConfig = {
-	API_DOMAIN: 'http://localhost:3000/api'
+const mode = 'dev';
+
+const configApi = () => {
+	if (mode === 'build') {
+		return { API_DOMAIN: 'https://inch-app.ru/api' }
+	} else if (mode === 'dev') {
+		return { API_DOMAIN: 'http://localhost:3000/api' }
+	}
 }
+
+const apiConfig = configApi()
 
 export default apiConfig;
