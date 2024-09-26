db804d8534b14637bbfbdc1e68e8a070ca3c6262
dev.inch <diomin.vd13@icloud.com>
2024-09-26 15:03:56 +0300

    replace notification (component) to src/UI/

---
+++ Client/src/ui/Notification/Notification.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+
+import IconNotification from '../../icons/IconNotification';
+
+import './Notification.scss';
+
+interface ComponentProps {
+	type: 'success' | 'error' | string;
+	title: string;
+	text: string;
+	visible?: boolean;
+}
+
+const Notification = ({ type, title, text, visible }: ComponentProps): JSX.Element => {
+	return (
+		<div className={`notification ${type} visible-${visible}`}>
+			<IconNotification size={6} />
+			<div className="notification-body">
+				<p className="notification-title">{title}</p>
+				<p className="notification-text">{text}</p>
+			</div>
+		</div>
+	)
+}
+
+export default Notification;
