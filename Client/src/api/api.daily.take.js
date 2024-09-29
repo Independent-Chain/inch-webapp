38a4f695d51f6f96db142cbe7f502889eaaaccb5
dev.inch <diomin.vd13@icloud.com>
2024-09-29 12:58:45 +0300

    [feat] Client API
    Added API files for interacting with the server

---
+++ Client/src/api/api.daily.take.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import API_CONFIG from './api.config.js';
+
+export const API_DAILY_TAKE = async (token, webApp) => {
+	const userId = webApp.initDataUnsafe.user.id
+	const options = {
+		method: 'GET',
+		url: `${API_CONFIG.API_DOMAIN}/daily/take/${userId}`,
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`,
+		}
+	};
+	try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+		throw error
+  }
+}
