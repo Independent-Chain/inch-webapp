b9a0a9a5d0f73d5101a3ba96c9b8581fd64095a0
dev.inch <diomin.vd13@icloud.com>
2024-10-04 21:44:50 +0300

    sadassadads

---
+++ Client/src/pages/Profile/modules/AboutUser/AboutUser.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutUser.scss';
+
+interface ComponentProps {
+
+}
+
+const AboutUser = ({  }: ComponentProps): JSX.Element => {
+	return (
+		<div className="about-user">
+			dasdsad
+		</div>
+	)
+}
+
+export default AboutUser;
